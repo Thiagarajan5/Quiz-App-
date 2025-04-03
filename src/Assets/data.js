c500ae4d49a044ac4820ae060b57a788307a5a7c
Thiagarajan5 <jairohith5@gmail.com>
2025-04-03 13:46:00 +0530

    First Commit

---
+++ src/Assets/data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+      question: "What is the capital of France?",
+      option1: "Berlin",
+      option2: "Madrid",
+      option3: "Paris",
+      option4: "Lisbon",
+      ans: 3,
+    },
+    {
+      question: "What is the largest planet in our Solar System?",
+      option1: "Earth",
+      option2: "Jupiter",
+      option3: "Saturn",
+      option4:  "Mars",
+      ans: 2,
+    },
+    {
+      question: "Who wrote 'To Kill a Mockingbird'?",
+      option1: "Harper Lee", 
+      option2: "Mark Twain", 
+      option3: "J.K. Rowling", 
+      option4: "Ernest Hemingway",
+      ans: 1,
+    },
+    {
+      question: "What is the speed of light?",
+      option1: "300,000 km/s", 
+      option2: "150,000 km/s", 
+      option3: "400,000 km/s",
+      option4: "500,000 km/s",
+      ans: 1,
+    },
+    {
+        question: "Which continent has the highest number of countries",
+        option1: "Asia", 
+        option2: "Europe", 
+        option3: "North America",
+        option4: "Africa",
+        ans: 4,
+    },
+  ];
+  
+ 
+  
